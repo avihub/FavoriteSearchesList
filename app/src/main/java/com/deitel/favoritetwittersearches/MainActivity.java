3aa2ef4228d975e99cc0e1a2c5d58d140760c6b8
avisbox <avisbox@gmail.com>
2015-07-24 14:29:48 +0300

    added functionality to onClick

--- app/src/main/java/com/deitel/favoritetwittersearches/MainActivity.java
+++ app/src/main/java/com/deitel/favoritetwittersearches/MainActivity.java
@@ -1,17 +1,38 @@
 package com.deitel.favoritetwittersearches;
 
+import android.app.Activity;
+import android.content.SharedPreferences;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TableLayout;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+
+    private SharedPreferences savedSearches;
+    private TableLayout queryTableLayout;
+    private EditText queryEditText;
+    private EditText tagEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        savedSearches = getSharedPreferences("searches", MODE_PRIVATE);
+        queryTableLayout = (TableLayout) findViewById(R.id.queryTableLayout);
+        queryEditText = (EditText) findViewById(R.id.queryEditText);
+        tagEditText = (EditText) findViewById(R.id.tagEditText);
+
+        Button saveButton = (Button) findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(saveButtonListener);
+        Button clearTagsButton = (Button) findViewById(R.id.clearTagsButton);
+
+        refreshButtons(null);
     }
 
     @Override
